832b4a53c241e4fa7bedfc5887e65878190e29de
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2022-03-03 17:09:28 -0800

    2022-3-3

--- practice_problems/Linked_List/Merge k Sorted Lists.py
+++ practice_problems/Linked_List/Merge k Sorted Lists.py
@@ -1,3 +1,5 @@
+from Queue import PriorityQueue
+
 class ListNode(object):
     def __init__(self, val = 0, next = None):
         self.val = val
@@ -23,3 +25,26 @@ class Solution(object):
             curr.next = ListNode(node)
             curr = curr.next
         return dummy.next
+
+    # Optimize by Priority Queue
+    # Time complexity O(N log K) where k is the number of linked lists.
+    # The comparison cost will be reduced to O(logk) for every pop and insertion to priority queue. But finding the node with the smallest value just costs O(1) time.
+    # There are N nodes in the final linked list.
+    # Space complexity O(N) creating a new linked list costs O(n) space
+    # O(k) The node above present applies in-place method which cost O(1) space.
+    # Priority queue costs O(k) space
+    def mergeKLists2(self, lists):
+        def mergeKLists(self, lists):
+            head = pointer = ListNode()
+            q = PriorityQueue()
+            for l in lists:
+                if l:
+                    q.put((l.val, l))
+            while not q.empty():
+                val, node = q.get()
+                pointer.next = ListNode(val)
+                point = point.next
+                node = node.next
+                if node:
+                    q.put((node.val, node))
+            return head.next
