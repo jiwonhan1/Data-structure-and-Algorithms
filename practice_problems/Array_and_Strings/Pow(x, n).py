3db2a6e5a64410afe6c5468ec24bee5f50c630ad
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2022-03-10 22:23:28 -0800

    2022-3-10

--- practice_problems/Array_and_Strings/Pow(x, n).py
+++ practice_problems/Array_and_Strings/Pow(x, n).py
@@ -1,6 +1,9 @@
 class Solution:
+    # Brute Force
     # Time complexity O(N)
     # Space complexity O(1)
+    # If n < 0, we can substitute x, n with 1/x, -n to make sure n >= 0
+    # We need to take care of the corner cases, especially different range limits for negative and positive integers
     def myPow(self, x, n):
         N = n
         if n == 0:
@@ -13,6 +16,14 @@ class Solution:
         for i in range(N):
             ans = ans * x
         return ans
+
+    # Fast Power Algorithm Recursive
+    # Assume we have got the result of x(n/2), and now we want to get the result of x(n).
+    # Let A be result of x(n/2), we can talk about x(n) based on the parity of n respectively
+    # If n is even, we can use formula xn(2) = x(2*n) to get x(n) = A*A. If n is odd, then A*A = x(n-1)
+    # Intuitively, we need to multiply another x to get the result, x(n) = A * A * x
+    # This approach can be easily implemented using recursion.
+    # We call this method "Fast Power", because we only need at most O(logn) computations to get x(n)
     # Time complexity O(logN)
     # Space complexity O(logN)
     def fastPow(self, x , n):
--- practice_problems/Trees_and_Graghs/Closest Binary Search Tree Value.py
+++ practice_problems/Trees_and_Graghs/Closest Binary Search Tree Value.py
@@ -5,6 +5,26 @@ class TreeNode:
         self.right = right
 
 class Solution:
+    # Iterative Inorder, O(k) time
+    # Initiate stack as an empty array and predecessor value as a very small number
+    # While root is not null:
+    # To build an inorder traversal iteratively, go left as far as you can and add all nodes on the way into stack
+    # Pop the last element from stack root = stack.pop()
+    # If target is in-between of pred and root.val, return the closest between these two elements
+    # Set predecessor value to be equal to root.val and go one step right: root= root.right
+    def closestValue(self, root, target):
+        stack, pred = [], float('-inf')
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+
+            if pred <= target and target < root.val:
+                return min(pred, root.val, key=lambda x: abs(target-x))
+            pred = root.val
+            root = root.right
+        return pred
     def closestValue(self, root, target):
         closest = root.val
         while root:
