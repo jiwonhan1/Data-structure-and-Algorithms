b80b2ad9e5ea2aa3d776d68836949df65014df2e
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2022-03-16 21:02:04 -0700

    2022-3-16

--- practice_problems/Array_and_Strings/Longest Substring Without Repeating Characters.py
+++ practice_problems/Array_and_Strings/Longest Substring Without Repeating Characters.py
@@ -1,4 +1,35 @@
 class Solution(object):
+
+    # Brute Force
+    # Check all the substring one by one to see if it has no duplicate character
+    # Time complexity O(N3)
+    # Space complexity O(min(n,m))
+    def lengthOfLongestSubstring(self, s):
+        def check(start,end):
+            chars = [0] * 26
+            for i in range(start, end+1):
+                c = s[i]
+                chars[ord[c] - ord['a']] += 1
+                if chars[ord[c]] > 1:
+                    return False
+            return True
+
+        n = len(s)
+        res = 0
+        for i in range(n):
+            for j in range(i, n):
+                if check(i, j):
+                    res = max(res, j-i+1)
+        return res
+
+    # Sliding Window
+    # A sliding window is an abstract concept commonly used in array/string problems
+    # A window is a range of elements in the array/string which usually defined by the start and end indices, i.e. [i,j]
+    # A sliding window is a window 'slides' its two boundaries to the certain direction
+    # For example, if we slide [i,j) to the right by 1 element, then it becomes [i+1, j+1)
+    # We use hashSet to store the characters in current window.
+    # Then we slide the index j to the right. If it is not in the HashSet, we slide j further.
+    # Doing so until s[j] is already in the HashSet
     # Time complextiy O(2n) = O(n)
     # Space complexity O(min(m,n))
 
