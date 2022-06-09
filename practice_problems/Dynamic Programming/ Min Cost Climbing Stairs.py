3ddc182d382cce6e2729dd60433927182e50439c
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2022-06-08 21:29:21 -0700

    2022-6-8

--- practice_problems/Dynamic Programming/ Min Cost Climbing Stairs.py
+++ practice_problems/Dynamic Programming/ Min Cost Climbing Stairs.py
@@ -1,3 +1,4 @@
+from functools import cache
 class Solution:
     # Bottom-up dynamic programming
     # Time and space complexity O(N)
@@ -22,4 +23,26 @@ class Solution:
             memo[i] = min(down_two, down_one)
             return memo[i]
         memo = {}
-        return minimum_cost(len(cost) )
+        return minimum_cost(len(cost))
+
+    def minCostClimbingStairs(self,cost):
+        # In Python, functools module contains functions that can be used to automatically memoize a function
+        @cache
+        def minimum_cost(i):
+            if i <=1: return 0
+
+            down_one = cost[i-1] + minimum_cost(i-1)
+            down_two = cost[i-2] + minimum_cost(i-2)
+            return min(down_one, down_two)
+        return minimum_cost(len(cost))
+
+    # Bottom-Up, Constant Space
+    # Time complexity O(N)
+    # Space complexity O(1)
+    def minCostClimbingStairs(self, cost):
+        down_one = down_two = 0
+        for i in range(2, len(cost) +1):
+            temp = down_one
+            down_one = min(down_one+cost[i-1],down_two+cost[i-2])
+            down_two = temp
+        return down_one
