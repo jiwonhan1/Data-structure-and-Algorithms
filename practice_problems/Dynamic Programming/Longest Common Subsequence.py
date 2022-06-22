447a1254a4568ddc2e724defcee2f0ea5798c544
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2022-06-21 23:28:39 -0700

    2022-6-21

--- practice_problems/Dynamic Programming/Longest Common Subsequence.py
+++ practice_problems/Dynamic Programming/Longest Common Subsequence.py
@@ -17,6 +17,30 @@ class Solution(object):
                 return max(memo_solve(p1, p2+1), memo_solve(p1+1, p2))
         return memo_solve(0,0)
 
+    # Memoization2
+    # Time complexity O(MN2)
+    # Space complexity O(MN)
+    def longestCommonSubsequence(self, text1, text2):
+        @cache(maxsize=None)
+        def memo_solve(p1, p2):
+            # Base case: If either string is now empty, we can't up anymore characters
+            if p1 == len(text1) or p2 == len(text2):
+                return 0
+
+            # Option 1: We don't include text[p1] in the solution.
+            option_1 = memo_solve(p1+1, p2)
+
+            # Option 2: We include text1[p1] in the solution, as long as a match for it in text2 at or after p2 exists
+            first_occurnece = text2.find(text1[p1], p2)
+            option_2 = 0
+            if first_occurnece != -1:
+                option_2 = 1 + memo_solve(p1+1, first_occurnece+1)
+
+            return max(option_1, option_2)
+
+        return memo_solve(0,0)
+
+
     # Dynamic Programming with 2D Array
     def longestCommonSubsequence(self, text1, text2):
         dp_grid = [[0] * (len(text2)+1) for _ in (len(text2)+1)]
